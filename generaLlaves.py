1a8f27ae968fe41b4dcfc1bd739d0f469990b3ab
acosatom <acosatom@gmail.com>
2020-06-01 18:16:34 +0200

    Add files via upload

---
+++ generaLlaves.py
@@ -0,0 +1,11 @@
+from TeKrypto import TeKrypto
+
+#Ver la documentación en la propia clase
+
+# Instancia la clase
+Crypto = TeKrypto()
+
+# Generar llaves (nombre de las llaves sin extensión y el tamaño de la llave)
+Crypto.generaLLaves('mi_llave_privada', 'mi_llave_publica', 4096)
+
+
