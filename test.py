90b5bfe3afe0f1228fcc63145a97707df97af6ef
acosatom <acosatom@gmail.com>
2020-06-01 18:15:06 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,29 @@
-from TeKrypto import Tekripto
+from TeKrypto import TeKrypto
 
 #Ver la documentación en la propia clase
 
 # Instancia la clase
 Crypto = TeKrypto()
 
-# Generar llaves (nombre de las llaves sin extensión y el tamaño de la llave)
-Crypto.generaLLaves('mi_llave_privada', 'mi_llave_publica', 4096)
 
 # Selecciona la llave pública con la que encriptar
-Crypto.usaLlave('mi_llave_publica.pem', 'public')
+#Crypto.usaLlave('mi_llave_publica.pem', 'public')
 
 # Encriptar un archivo 
-Crypto.encriptaArchivo("archivo.pdf", False)
+#Crypto.encriptaArchivo("test/test.jpg", False)
 
 # Encriptar un directorio
-Crypto.encriptaDirectorio("directorio", False)
+#Crypto.encriptaDirectorio("data/Documentos", False)
 
 
 # Selecciona la llave privada con la que desencriptar
 Crypto.usaLlave('mi_llave_privada.pem', 'private')
 
 # Desencripta un archivo
-Crypto.desencriptaArchivo("archivo.pdf.crypt", False)
+Crypto.desencriptaArchivo("test/test.jpg.crypt", False)
 
 # Desencripta un direcotrio
-crypt.desencriptaDirectorio("data/Documentos", False)
+#crypt.desencriptaDirectorio("data/Documentos", False)
 
 # Sube directorio/archivo por FTP
-Crypto.ftp("data/")
+#Crypto.ftp("data/")
